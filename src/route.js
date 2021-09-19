564dd5be439ce2effd21978221f298472d65ea64
Wesley Guerra <guerra_ssa@hotmail.com>
2021-09-19 13:54:40 -0300

    Criando components para reaproveitar codigo

--- src/route.js
+++ src/route.js
@@ -2,9 +2,11 @@ const express = require("express");
 const QuestionController = require("./controllers/QuestionController");
 const route = express.Router(); //função de criar rotas
 
-route.get("/", (req, res) => res.render("index"));
+route.get("/", (req, res) => res.render("index", { page: "enter-room" }));
+route.get("/create-pass", (req, res) =>
+  res.render("index", { page: "create-pass" })
+);
 route.get("/room", (req, res) => res.render("room"));
-route.get("/create-pass", (req, res) => res.render("create-pass"));
 
 //formato que o formulário de dentro do modal tem que passar a informação
 route.post("/question/:room/:question/:action", QuestionController.index);
