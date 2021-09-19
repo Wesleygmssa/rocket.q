83d0cf4e562ce13934c582c6b499c76e6dd539c1
Wesley Guerra <guerra_ssa@hotmail.com>
2021-09-19 14:23:04 -0300

    Configurando os diretorios corretos

--- src/route.js
+++ src/route.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const QuestionController = require("./controllers/QuestionController");
+const RoomController = require("./controllers/RoomController");
 const route = express.Router(); //função de criar rotas
 
 route.get("/", (req, res) => res.render("index", { page: "enter-room" }));
 route.get("/create-pass", (req, res) =>
   res.render("index", { page: "create-pass" })
 );
-route.get("/room", (req, res) => res.render("room"));
+route.get("/room/:room", (req, res) => res.render("room"));
 
 //formato que o formulário de dentro do modal tem que passar a informação
 route.post("/question/:room/:question/:action", QuestionController.index);
+route.post("/room/create-room", RoomController.create);
 
 module.exports = route; // exportando rotas
