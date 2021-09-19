fa8c68a10a7639db0456a19fc297e33779d53849
Wesley Guerra <guerra_ssa@hotmail.com>
2021-09-19 10:48:06 -0300

    Construindo modal com javascript

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,43 +1,48 @@
-import Modal from './modal.js'
+import Modal from "./modal.js";
 
-const modal = Modal()
+const modal = Modal();
 
-const modalTitle = document.querySelector('.modal h2')
-const modalDescription = document.querySelector('.modal p')
-const modalButton = document.querySelector('.modal button')
-
+const modalTitle = document.querySelector(".modal h2");
+const modalDescription = document.querySelector(".modal p");
+const modalButton = document.querySelector(".modal button");
 
 //Pegar todos os botões que existe com a classe check
-const checkButtons = document.querySelectorAll(".actions a.check") 
+const checkButtons = document.querySelectorAll(".actions a.check");
 
-checkButtons.forEach(button => {
-    //adicionar a escuta
-    button.addEventListener("click", handleClick)
-})
-
+checkButtons.forEach((button) => {
+  //adicionar a escuta
+  button.addEventListener("click", handleClick);
+  //   button.addEventListener("click", () => {
+  //     modal.open();
+  //   });
+  //   modal.open();
+});
 
 /*Quando o botão delete for clicado ele abre a modal */
-const deleteButton = document.querySelectorAll(".actions a.delete")
+const deleteButton = document.querySelectorAll(".actions a.delete");
 
-deleteButton.forEach(button => {
-    button.addEventListener("click", (event) => handleClick(event, false))
-})
+deleteButton.forEach((button) => {
+  button.addEventListener("click", (event) => handleClick(event, false));
+});
 
-function handleClick(event, check = true){
-    event.preventDefault()
-    const text = check ? "Marcar como lida" : "Excluir"
-    const slug = check ? "check" : "delete"
-    const roomId = document.querySelector("#room-id").dataset.id
-    const questionId = event.target.dataset.id
+function handleClick(event, check = true) {
+  event.preventDefault();
+  //verificção para mudar o dexto de acordo ao compente selecionado
+  const text = check ? "Marcar como lida" : "Excluir"; // ternario
+  const slug = check ? "check" : "delete";
+  const roomId = document.querySelector("#room-id").dataset.id;
+  const questionId = event.target.dataset.id; // o elemento TARGET que aconteceu o evento
 
-    const form = document.querySelector(".modal form")
-    form.setAttribute("action", `/question/${roomId}/${questionId}/${slug}`)
+  const form = document.querySelector(".modal form");
+  form.setAttribute("action", `/question/${roomId}/${questionId}/${slug}`);
 
-    modalTitle.innerHTML= `${text} esta pergunta`
-    modalDescription.innerHTML= `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`
-    modalButton.innerHTML= `Sim, ${text.toLowerCase()}`
-    check ? modalButton.classList.remove("red") : modalButton.classList.add("red")
-    
-    //abrir modal
-    modal.open()
+  modalTitle.innerHTML = `${text} esta pergunta`;
+  modalDescription.innerHTML = `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`;
+  modalButton.innerHTML = `Sim, ${text.toLowerCase()}`;
+  check
+    ? modalButton.classList.remove("red")
+    : modalButton.classList.add("red");
+
+  //abrir modal
+  modal.open();
 }
--- src/route.js
+++ src/route.js
@@ -5,4 +5,9 @@ route.get("/", (req, res) => res.render("index"));
 route.get("/room", (req, res) => res.render("room"));
 route.get("/create-pass", (req, res) => res.render("create-pass"));
 
+//formato que o formulário de dentro do modal tem que passar a informação
+route.get("/room/:room/:question/:action", (req, res) =>
+  res.render("exemple", { req })
+);
+
 module.exports = route; // exportando rotas
