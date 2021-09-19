b3b1fe62bea20604ca0df81ce5a017bfb0a41e7a
Wesley Guerra <guerra_ssa@hotmail.com>
2021-09-19 16:36:16 -0300

    Configurando conecxão com banco

--- src/db/config.js
+++ src/db/config.js
@@ -1,9 +1,18 @@
 const sqlite3 = require("sqlite3");
 const { open } = require("sqlite"); //abrir uma conexão com banco de dados
-
+//OBS se ligar no retorno da função
 module.exports = () => {
-  open({
-    filename: "./src/db/rockeatq.sqlite",
+  return open({
+    filename: "./src/db/rocketq.sqlite",
     driver: sqlite3.Database,
   });
 };
+
+// const sqlite3 = require("sqlite3");
+// const { open } = require("sqlite");
+
+// module.exports = () =>
+//   open({
+//     filename: "./src/db/rocketq.sqlite",
+//     driver: sqlite3.Database,
+//   });
--- src/db/init.js
+++ src/db/init.js
@@ -14,7 +14,6 @@ const initDb = {
     )`);
 
     await db.close();
-    d;
   },
 };
 
