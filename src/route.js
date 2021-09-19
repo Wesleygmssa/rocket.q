b96c752ad2bacb4760b37b5ac5b7d8d045dc2b0e
Wesley Guerra <guerra_ssa@hotmail.com>
2021-09-19 14:54:35 -0300

    Ajustando rotas

--- src/route.js
+++ src/route.js
@@ -4,13 +4,16 @@ const RoomController = require("./controllers/RoomController");
 const route = express.Router(); //função de criar rotas
 
 route.get("/", (req, res) => res.render("index", { page: "enter-room" }));
+
 route.get("/create-pass", (req, res) =>
   res.render("index", { page: "create-pass" })
 );
+
 route.get("/room/:room", (req, res) => res.render("room"));
 
-//formato que o formulário de dentro do modal tem que passar a informação
 route.post("/question/:room/:question/:action", QuestionController.index);
-route.post("/room/create-room", RoomController.create);
+route.post("/create-room", RoomController.create);
+
+// route.post("/create-room", (req, res) => res.render("room"));
 
 module.exports = route; // exportando rotas
