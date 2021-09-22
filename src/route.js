8fbd3f17dd7f63cb1093f687f9742ae2189095a2
wesley guerra <wesleyguerra9@gmail.com>
2021-09-22 14:01:07 -0300

    Finalizando projeto

--- src/route.js
+++ src/route.js
@@ -10,8 +10,16 @@ route.get("/create-pass", (req, res) =>
 
 //Formato que o formulário de dentro do modal tem que passar a informação
 route.get("/room/:room", RoomController.open);
+
+//Criando sala
 route.post("/create-room", RoomController.create);
 
+//Entrando uma sala existente
+route.post("/enterroom", RoomController.enter);
+
+//Criando pergunta
 route.post("/question/create/:room", QuestionController.create);
+
+//Verificando a ação que será executada na pergunta
 route.post("/question/:room/:question/:action", QuestionController.index);
 module.exports = route; // exportando rotas
