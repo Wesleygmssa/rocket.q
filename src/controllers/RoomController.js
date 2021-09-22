e9f003e9002e172fc46313be739f85ff964b32c0
Wesley Guerra <guerra_ssa@hotmail.com>
2021-09-21 23:05:02 -0300

    update

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -50,6 +50,13 @@ module.exports = {
     const questions = await db.all(
       `SELECT * FROM questions WHERE room = ${roomId}`
     );
-    res.render("room", { roomId: roomId, questions: questions });
+    const questionsRead = await db.all(
+      `SELECT * FROM questions WHERE room = ${roomId} and read = 1`
+    );
+    res.render("room", {
+      roomId: roomId,
+      questions: questions,
+      questionsRead: questionsRead,
+    });
   },
 };
